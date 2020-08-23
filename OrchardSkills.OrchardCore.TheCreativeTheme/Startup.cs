65464d538963bff14c7df1022038702871d1cf1a
sales <sales@orchardskills.com>
2020-08-23 08:00:04 -0600

    Uodated theme

---
+++ OrchardSkills.OrchardCore.TheCreativeTheme/Startup.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
+
+namespace OrchardCore.Themes.TheCreativeTheme
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
+        }
+    }
+}
